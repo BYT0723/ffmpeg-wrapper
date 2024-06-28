b478264e5ae45de7f75195cbc23e4181f60b8c1f
BYT0723 <twang9739@gmail.com>
2024-06-28 18:21:57 +0800

    chore: ffmpeg.sh docu

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -13,10 +13,16 @@ camera_device=/dev/video0
 camera_size=320x240
 camera_rate=30
 camera_position_x=W-w-100
-camera_position_y=0
-# camera_position_y=H-h-1280
-
+camera_position_y=H-h
 # echo $(v4l2-ctl --list-formats-ext -d "$CAMERA")
+
+ffmpeg \
+	-f x11grab -s 2240x1400 -r 144 -i $DISPLAY \
+	-f v4l2 -video_size 320x240 -r 30 -i /dev/video0 \
+	-f pulse -i "$(pactl get-default-sink).monitor" \
+	-f pulse -i "$(pactl get-default-source)" \
+	-filter_complex "[0:v]scale=1920:1200[scaled];[scaled][1:v]overlay=W-w-100:H-h" \
+	-c:v libx264 -c:a aac -preset superfast -tune zerolatency -threads "$(echo "$(nproc)/2" | bc)" -bufsize 2M -rtbufsize 100M -f mp4 output.mp4
 
 audio_output_device="$(pactl get-default-sink).monitor"
 audio_input_device="$(pactl get-default-source)"
